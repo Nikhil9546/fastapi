86a50751e0e25d230b42ada376008bcaae990bda
NIKHIL KUMAR <65764574+Nikhil9546@users.noreply.github.com>
2023-12-07 02:41:02 +0530

    Update applications.py

--- fastapi/applications.py
+++ fastapi/applications.py
@@ -1,4 +1,4 @@
-#testing on cloud -- 2344 -45  -67556 dkabkvf dfghfhy tg fhnhgjhg dfbgbgn
+#testing on cloud -- 2344 -45  -67556 dkabkvf dfghfhy tg fhnhgjhg dfbgbgn jjvvjhhc
 from enum import Enum
 from typing import (
     Any,
