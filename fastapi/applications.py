6668d7bfd519a7d4a2e4bc0aaf6e002ca483fa00
NIKHIL KUMAR <65764574+Nikhil9546@users.noreply.github.com>
2023-12-11 01:59:49 +0530

    Update applications.py

--- fastapi/applications.py
+++ fastapi/applications.py
@@ -1,4 +1,4 @@
-#testing on cloud -- 2344 -45  -67556 dkabkvf dfghfhy fgdhryytjy fdgrhy hjfjyfy dhrtyy dfghyy
+#testing on cloud -- 2344 -45  -67556 dkabkvf dfghfhy fgdhryytjy fdgrhy hjfjyfy dhrtyy dfghyy dhyrhyju
 from typing import (
     Any,
     Awaitable,
